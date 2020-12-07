8b2864b0b183e0dc5e5aeb0522f75a8acdfd14c5
Diego de jesus Cordeiro <diego.alura@gmail.com>
2020-12-07 20:20:40 -0300

    Commit initial.

---
+++ src/test/java/br/com/timesheetio/sheet/SheetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.timesheetio.sheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
